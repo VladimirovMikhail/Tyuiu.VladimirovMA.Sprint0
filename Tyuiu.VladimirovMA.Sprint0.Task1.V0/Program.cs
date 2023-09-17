dae90cc2b4b6961e83553522fad1e1dc7f955515
Tyuiu.VladimirovMA@outlook.com <Tyuiu.VladimirovMA@outlook.com>
2023-09-17 14:13:10 +0500

    added Task1

---
+++ Tyuiu.VladimirovMA.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.VladimirovMA.Sprint0.Task1.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            String name = "Михаил";
+            int age = 45;
+            Console.WriteLine("Здравствуйте, меня зовут: "+ name +" мне " + age + "лет");
+            Console.ReadKey();
+        }
+    }
+}
