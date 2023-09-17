ce5a58e061335fbde9f49216fd89018571d8c417
Tyuiu.VladimirovMA@outlook.com <Tyuiu.VladimirovMA@outlook.com>
2023-09-17 15:39:04 +0500

    added Task3

---
+++ Tyuiu.VladimirovMA.Sprint0.Task3.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.VladimirovMA.Sprint0.Task3.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.VladimirovMA.Sprint0.Task3.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5737a7bc-ce70-436d-a0bc-fb3a3fbd0961")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.VladimirovMA.Sprint0.Task3.V0.Lib/DataService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.VladimirovMA.Sprint0.Task3.V0.Lib
+{
+    public class DataService
+    {
+        public static int Sum(int x, int y)
+        {
+            return (x + y);
+        }
+    }
+}
